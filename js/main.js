af0de16e011128a2db9e762fe22e07131a485756
vararie <jinks@superd.tw>
2015-03-18 16:37:36 -0700

    tail

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-var star =[];
+var snow =[];
 var t = [];
+var star =[];
 
 function preload(){
 
@@ -7,16 +8,23 @@ function preload(){
 
 function setup(){
 	createCanvas(windowWidth,windowHeight);
-	star = new Star(100);
-	star.updatePos();
+	snow = new Snow(100);
+	snow.updatePos();
 	t = new Tree();
+
+	star = new Star(10);
+	star.updatePos();
 }
 
 function draw(){
 	background(0);
 	t.appear();
 	
-	// stars
+	// Snows
+
+	// snow.appear();
+	// snow.fall();
+	// snow.updatePosWithIndex(snow.checkDisappear());	
 
 	star.appear();
 	star.fall();
@@ -46,6 +54,88 @@ Tree.prototype.appear = function(){
 	triangle(this.p1.x,this.p1.y,this.p2.x,this.p2.y,this.p4.x,this.p4.y);
 }
 
+// ********** Snow class **********
+
+function Snow(amount){
+	this.x = new Array(amount);
+	this.y = new Array(amount);
+	this.xSpeed = new Array(amount);
+	this.ySpeed = new Array(amount);
+	this.amount = amount;
+	this.sOpac = 255;
+	this.size  = new Array(amount);
+	this.so = 150;
+}
+
+Snow.prototype.appear = function(){
+	for(i=0;i<this.amount;i++){
+		var sw = random(5);
+		this.so = random(100,200);
+		stroke(255,this.so);
+		strokeWeight(sw);  // stroke is random
+		fill(255,255,255,this.sOpac); // middle always white
+		ellipse(this.x[i],this.y[i],this.size[i],this.size[i]);
+	}
+}
+
+Snow.prototype.updatePos = function(){
+	this.sOpac = 255;
+	for(i=0;i<this.amount;i++){
+		this.x[i] = random(width);
+		this.y[i] = random(height);
+		this.size[i] = 5;
+	}
+	for(i=0;i<this.amount;i++){
+		this.xSpeed[i] = (random(10));
+		this.ySpeed[i] = (random(10));
+	}
+}
+
+Snow.prototype.fall = function(){
+	for(i=0;i<this.amount;i++){
+		this.x[i] += this.xSpeed[i];
+		this.y[i] += this.ySpeed[i]; 
+	}
+}
+
+Snow.prototype.checkDisappear = function(){
+	for(i=0;i<this.amount;i++){
+		if (this.x[i] < 0 || this.x[i] > width || this.y[i] < 0 || this.y[i] > height) {
+			return i;
+		}
+	}
+}
+
+Snow.prototype.updatePosWithIndex = function(i){
+	this.x[i] = random(width);
+	this.y[i] = random(height);
+	this.size[i] = 5;
+}
+
+// Snow.prototype.updatePosLeftWithIndex = function(i){
+// 	this.x[i] = random((width/2)/2);
+// 	this.y[i] = random(height);
+// 	this.size[i] = 5;
+// }
+
+// Snow.prototype.appearInMotion = function(){
+// 	for(i=0;i<this.amount;i++){
+// 		var sw = random(5);
+// 		var so = random(100,200);
+// 		stroke(255,so);
+// 		strokeWeight(sw);
+// 		fill(255,255,255,this.sOpac);
+// 		rect(this.x[i],this.y[i],100,1);
+// 	}
+// }
+
+// Snow.prototype.move = function(){
+// 	for(i=0;i<this.amount;i++){
+// 		this.x[i] += (this.xSpeed[i]) +25;
+// 	}	
+// }
+
+
 // ********** Star class **********
 
 function Star(amount){
@@ -70,6 +160,15 @@ Star.prototype.appear = function(){
 	}
 }
 
+Star.prototype.tail = function(){  // the tail for the shooting stars
+	for(i=0; i<this.amount; i++){
+		fill(255,255,255,this.sOpac);
+		ellipse(this.x[i],this.y[i],size[i],this.size[i]);
+
+	}
+
+}
+
 Star.prototype.updatePos = function(){
 	this.sOpac = 255;
 	for(i=0;i<this.amount;i++){
@@ -78,8 +177,8 @@ Star.prototype.updatePos = function(){
 		this.size[i] = 5;
 	}
 	for(i=0;i<this.amount;i++){
-		this.xSpeed[i] = (random(10))-5;
-		this.ySpeed[i] = (random(10))-5;
+		this.xSpeed[i] = 10;
+		this.ySpeed[i] = 10;
 	}
 }
 
@@ -98,31 +197,8 @@ Star.prototype.checkDisappear = function(){
 	}
 }
 
-// Star.prototype.updatePosWithIndex = function(i){
-// 	this.x[i] = random(width);
-// 	this.y[i] = random(height);
-// 	this.size[i] = 5;
-// }
-
-// Star.prototype.updatePosLeftWithIndex = function(i){
-// 	this.x[i] = random((width/2)/2);
-// 	this.y[i] = random(height);
-// 	this.size[i] = 5;
-// }
-
-// Star.prototype.appearInMotion = function(){
-// 	for(i=0;i<this.amount;i++){
-// 		var sw = random(5);
-// 		var so = random(100,200);
-// 		stroke(255,so);
-// 		strokeWeight(sw);
-// 		fill(255,255,255,this.sOpac);
-// 		rect(this.x[i],this.y[i],100,1);
-// 	}
-// }
-
-// Star.prototype.move = function(){
-// 	for(i=0;i<this.amount;i++){
-// 		this.x[i] += (this.xSpeed[i]) +25;
-// 	}	
-// }
+Star.prototype.updatePosWithIndex = function(i){
+	this.x[i] = random(width);
+	this.y[i] = random(height);
+	this.size[i] = 5;
+}
