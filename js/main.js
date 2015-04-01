b31a63122af963d153ec77a0ffd807bfc93b3eae
vararie <jinks@superd.tw>
2015-04-01 02:35:09 -0700

    iceberg changes color and parallax function

--- js/main.js
+++ js/main.js
@@ -4,14 +4,14 @@ var star =[];
 var water = [];
 var aur = [];
 
-var landscape1;
+// var landscape1;
 var landscape2;
 var theX;
 
 
 function preload() {
-  landscape1 = loadImage("img/landscape1.svg");
-  landscape2 = loadImage("img/landscape2.svg");
+  landscape_front = loadImage("img/landscape_front.svg");
+  landscape_back = loadImage("img/landscape_back.svg");
 }
 
 
@@ -33,78 +33,80 @@ function setup(){
 	water = new Water(2000);
 	water.init();
 
-<<<<<<< HEAD
-=======
 	// paper.setup(document.getElementById('defaultCanvas'));
 	// landscape = paper.project.importSVG(document.getElementById('landscape'));
  //    landscape.position = createVector(windowWidth/2,windowHeight/2);
->>>>>>> origin/master
 
     theX = 0;
 };
 
 function draw(){
-<<<<<<< HEAD
-	background();
-=======
+
 	// rectMode(CENTER);
 	
 		background(35,30,70);
 
+	push();
+	aur.appear();
+	aur.move();
+	pop();
+	
+	fill(50,190,230,255);
+	rect(0,height/2-20,2500,height);
+	
 	// for(i=0;i<height;i++){
 	// 	stroke(i/4,0,0);
 	// 	line(0,i,width,i);
 	// }
->>>>>>> origin/master
 
-	if(mouseX < (width/2)/2){
-		theX -= 10;
-	}
-	else if (mouseX > (width/2) + ((width/2)/2)){
-		theX += 10;
-	}
-	else{
-		theX = theX;
-	}
-		push();
-		translate(theX,0);
-<<<<<<< HEAD
-
-	image(landscape1, 300, 0, width, height);
-	image(landscape2, -300, 0, width, height);
-
-	ice.appear();
-=======
+	// if(mouseX < (width/2)/2){
+	// 	theX -= 10;
+	// }
+	// else if (mouseX > (width/2) + ((width/2)/2)){
+	// 	theX += 10;
+	// }
+	// else{
+	// 	theX = theX;
+	// }
 	
->>>>>>> origin/master
-	
-	// Snows
-
-	// snow.appear();
-	// snow.fall();
-	// snow.updatePosWithIndex(snow.checkDisappear());	
 
 	star.appear();
 	star.fall();
 	star.updatePosWithIndex(star.checkDisappear());
 	star.tail();
 
-	aur.appear();
-	aur.move();
-	//aur.updateLen();
+	var x3 = map(mouseX, 0, width, -300, width+300); // single iceburg
+	var x1 = map(mouseX, 0, width, 0, width); // front
+	var x2 = map(mouseX, 0, width, 300, width-300); // back
 
+	push();
+	translate(-x2/4,0);
+	image(landscape_back, 0, height/8, 1700, height-height/4);
+	pop();
+
+	push();
+	translate(-x1/2,0); 
+	image(landscape_front, 300, 0, 2000, height-height/4);
+	pop();
+
+	push();
+	translate(-x3,0); 
 	ice.appear();
+	pop();
+	
 
+	// Snows
+
+	// snow.appear();
+	// snow.fall();
+	// snow.updatePosWithIndex(snow.checkDisappear());	
+	//aur.updateLen();
 	water.appear();
 	water.flow();
 	//water.reFlow(water.checkDisappear());
 
-<<<<<<< HEAD
-=======
-
-
 	//landscape.view.update();
->>>>>>> origin/master
-		pop();
+
+	
 	
 };
